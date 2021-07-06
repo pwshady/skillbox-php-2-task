b0134e6cee44162bda4019b690c1f6a27314e054
pwshady <pwshady@gmail.com>
2021-07-06 20:59:53 +0300

    task1-2

--- task1.php
+++ task1.php
@@ -24,17 +24,57 @@ class Animal
 
 class Cat extends Animal
 {
+    public $animalColor = "";
+    public $favoriteFood = "";
 
+    public function __construct($name, $animalColor, $favoriteFood)
+    {
+        parent::__construct($name);
+        $this->setAnimalColor($animalColor);
+        $this->setFavoriteFood($favoriteFood);
+    }
+
+    public function getAnimalColor()
+    {
+        return $this->animalColor;
+    }
+
+    public function setAnimalColor($animalColor)
+    {
+        $this->animalColor = $animalColor;
+    }
+
+    public function getFavoriteFood()
+    {
+        return $this->favoriteFood;
+    }
+
+    public function setFavoriteFood($favoriteFood)
+    {
+        $this->favoriteFood = $favoriteFood;
+    }
 }
 
-class Dog
+class Dog extends Animal
 {
 
 }
 
-class Fish
+class Fish extends Animal
 {
 
+}
+
+class HungryCat extends Cat
+{
+    public function eat($food)
+    {
+        $catMessage = "Hungry cat: " . $this->getName() . ", special features: color - " . $this->getAnimalColor() . ", ate " . $food;
+        if ($this->getFavoriteFood() === $food){
+            $catMessage .= " and purred 'mrrrrr' from his favorite food.";
+        }
+        print ("<p>" . $catMessage . "</p>");
+    }
 }
 
 ?>
@@ -43,9 +83,9 @@ class Fish
 
 $zoo = [];
 
-$zoo[0] = new Cat("Cat1");
-$zoo[1] = new Cat("Cat2");
-$zoo[2] = new Cat("Cat3");
+//$zoo[0] = new Cat("Cat1");
+//$zoo[1] = new Cat("Cat2");
+//$zoo[2] = new Cat("Cat3");
 
 $zoo[3] = new Dog("Dog1");
 $zoo[4] = new Dog("Dog2");
@@ -55,6 +95,7 @@ $zoo[7] = new Dog("Dog5");
 
 $zoo[8] = new Fish("Fish1");
 
-
-var_dump($zoo);
+$hungryCat = new HungryCat("musja", "black", "milk");
+$hungryCat->eat("milk");
+var_dump($hungryCat);
 ?>

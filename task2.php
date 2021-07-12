47895f28ddbf09990cbb2252275c00af942685c4
pwshady <pwshady@gmail.com>
2021-07-12 21:54:00 +0300

    task1-2-2

--- task2.php
+++ task2.php
@@ -2,7 +2,49 @@
 
 namespace Factory;
 
-class Tuy
+class Toy
 {
-    public $nameToy
+    public $nameToy = "";
+    public $priceToy = 0;
+
+    public function getNameToy()
+    {
+        return $this->nameToy;
+    }
+
+    public function setNameToy($nameToy)
+    {
+        $this->nameToy = $nameToy;
+    }
+
+    public function getPriceToy()
+    {
+        return $this->priceToy;
+    }
+
+    public function setPriceToy($priceToy)
+    {
+        $this->priceToy = $priceToy;
+    }
 }
+
+class ToyFactory extends Toy
+{
+    public function __construct($nameToy, $priceToy)
+    {
+        $this->setNameToy($nameToy);
+        $this->setPriceToy($priceToy);
+    }
+}
+
+$numberOfToy = rand(5,20);
+$totalPrice = 0;
+
+for ($i = 0; $i < rand(5,20); $i++){
+    $toy = new ToyFactory("toy", rand(0,100));
+    $price = $toy->getPriceToy();
+    $totalPrice += $price;
+    print ("<p> Toy number: " . $i . " - Price: " . $price . "</p>");
+};
+
+print ("<p>Total toys price: " . $totalPrice . "</p>");
